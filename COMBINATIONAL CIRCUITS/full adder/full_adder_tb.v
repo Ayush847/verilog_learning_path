ce94c349ed95699aef09f2ac4e3e4fe5b77d7363
ayush <118741345+Ayush847@users.noreply.github.com>
2025-08-19 00:26:10 +0530

    Update full_adder_tb.v

--- COMBINATIONAL CIRCUITS/full adder/full_adder_tb.v
+++ COMBINATIONAL CIRCUITS/full adder/full_adder_tb.v
@@ -10,29 +10,23 @@ module full_adder_tb;
     .carry(carry));
   
   initial begin
-    $monitor("x=%b, y=%b, z=%b, sum=%b, carry=%b", x,y,sum,carry);
+    $monitor("x=%b, y=%b, z=%b, sum=%b, carry=%b", x,y,z,sum,carry);
+    
     x=0; y=0; z=0;
     #10;
-    
     x=0; y=0; z=1;
     #10;
-    
     x=0; y=1; z=0;
     #10;
-    
     x=0; y=1; z=1;
     #10;
-    
     x=1; y=0; z=0;
     #10;
-    
-    x=1; y=0; y=1;
+    x=1; y=0; z=1;
     #10;
-    
-    x=1; y=1; y=0;
+    x=1; y=1; z=0;
     #10;
-    
-    x=1; y=1; y=1;
+    x=1; y=1; z=1;
     #10;
     
     $finish;
