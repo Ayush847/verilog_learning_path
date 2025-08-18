843b73e902727c8eeff233082512f27a10713296
ayush <118741345+Ayush847@users.noreply.github.com>
2025-08-18 22:30:14 +0530

    Create nand_gate_tb.v

---
+++ nand_gate/nand_gate_tb.v
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module nand_gate_tb;
+  reg x;
+  reg y;
+  wire z;
+  
+  nand_gate DUT(
+    .a(x),
+    .b(y),
+    .c(z));
+  initial begin
+  	x=1'b0; y=1'b0;
+  	#10;
+  	x=1'b0; y=1'b1;
+  	#10;
+  	x=1'b1; y=1'b0;
+  	#10;
+  	x=1'b1; y=1'b1;
+  	#20;
+    $finish;
+  end
+  initial begin
+    $display("Time | a | b | c");
+    $monitor("%0t | %b | %b |%b", $time, x,y,z);
+  end
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0, nand_gate_tb);
+  end
+endmodule
